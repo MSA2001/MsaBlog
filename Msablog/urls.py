ec6423ef0bbd14fd7a84f4cb77dcedc8f4363b24
Msa2001 <msa2001b21gmail.com>
2022-08-24 20:47:43 +0100

    Login templates added

--- Msablog/urls.py
+++ Msablog/urls.py
@@ -18,6 +18,7 @@ from django.urls import path,include
 
 
 urlpatterns = [
+    path('',include('home.urls')),
+    path('',include('accounts.urls')),
     path('admin/', admin.site.urls),
-    path('',include('home.urls'))
 ]
