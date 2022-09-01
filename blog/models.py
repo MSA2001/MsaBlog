267625861197fac84437e134c0d1bd05fe610ae4
Msa2001 <msa2001b21gmail.com>
2022-09-01 23:02:32 +0100

    Article models created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
+from turtle import title
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=70)
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/articles')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    
+    def __str__(self):
+        return f"{self.title} - {self.body[:30]} "
