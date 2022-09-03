7f0b6017312ebe01b9f5c9c15d5ac55a149f079a
Msa2001 <msa2001b21gmail.com>
2022-09-03 21:26:49 +0100

    Article models updated,Media root&urls added

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,13 @@
+from tkinter import CASCADE
 from turtle import title
 from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Article(models.Model):
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
     title = models.CharField(max_length=70)
     body = models.TextField()
     image = models.ImageField(upload_to='images/articles')
