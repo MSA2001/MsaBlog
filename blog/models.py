7eeb9345a6e9b969a88a1bf5189d94de8ecdc4c9
Msa2001 <msa2001b21gmail.com>
2022-09-04 21:00:32 +0100

    Category added...

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,24 @@
 from tkinter import CASCADE
 from turtle import title
+from unicodedata import category
 from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth.models import User
 
 
 # Create your models here.
+class Category(models.Model):
+    title=models.CharField(max_length=100)
+    created=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+
+
+
 class Article(models.Model):
     author = models.ForeignKey(User,on_delete=models.CASCADE)
+    category=models.ManyToManyField(Category)
     title = models.CharField(max_length=70)
     body = models.TextField()
     image = models.ImageField(upload_to='images/articles')
