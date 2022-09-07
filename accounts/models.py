03c603361272fd22e8084317f331a4d397acf1c2
Msa2001 <msa2001b21gmail.com>
2022-09-07 21:58:04 +0100

    profile.EmailField added

--- accounts/models.py
+++ accounts/models.py
@@ -6,8 +6,7 @@ from django.contrib.auth.models import User
 
 class profile(models.Model):
     user =models.OneToOneField(User,on_delete=models.CASCADE)
-    melicode=models.CharField(max_length=12)
-    fathersname=models.CharField(max_length=20)
+    email =models.EmailField(null=True,blank=True)
     image=models.ImageField(upload_to='profile/image',blank=True,null=True)
     
     def __str__(self):
