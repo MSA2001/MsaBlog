0e778c3e48181fed3ed975cb7dfe3aacd576040a
Msa2001 <msa2001b21gmail.com>
2022-09-09 12:15:55 +0100

    detail to home url added

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Article
 # Create your views here.
 def post_detail(request,pk):
-    article = Article.objects.get(id=pk)
+    article = get_object_or_404(Article,id=pk)
     return render (request,'blog/post-details.html',{ 'article':article })
