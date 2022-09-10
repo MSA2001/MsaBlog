e0a5041f4c4001c54dba8c94fff10696e3285fd0
Msa2001 <msa2001b21gmail.com>
2022-09-10 18:05:12 +0100

    Slug added/title to slug

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from unicodedata import category
 from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -21,11 +22,15 @@ class Article(models.Model):
     author = models.ForeignKey(User,on_delete=models.CASCADE)
     category=models.ManyToManyField(Category)
     title = models.CharField(max_length=70)
+    slug =models.SlugField(blank=True,unique=True)
     body = models.TextField()
     image = models.ImageField(upload_to='images/articles')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        self.slug=slugify(self.title)
+        super(Article, self).save()
     
     def __str__(self):
         return f"{self.title} - {self.body[:30]} "
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Article
 # Create your views here.
-def post_detail(request,pk):
-    article = get_object_or_404(Article,id=pk)
+def post_detail(request,slug):
+    article = get_object_or_404(Article,slug=slug)
     return render (request,'blog/post-details.html',{ 'article':article })
