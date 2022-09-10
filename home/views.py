e606a1df04f21824bedc4fb7141ff4e3c9f9b836
Msa2001 <msa2001b21gmail.com>
2022-09-10 19:22:08 +0100

    recent post function added to home and detail page

--- home/views.py
+++ home/views.py
@@ -3,4 +3,6 @@ from blog.models import Article
 # Create your views here.
 def home(request):
     articles = Article.objects.all()
-    return render(request,'home/index.html',{ 'articles':articles })
+    recent_articles=Article.objects.all().order_by('-updated')[:4]
+
+    return render(request,'home/index.html',{ 'articles':articles , 'recent_articles':recent_articles })
