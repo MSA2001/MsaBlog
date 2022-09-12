bab66496b77084ae28166cf5585614e7dbc4553c
Msa2001 <msa2001b21gmail.com>
2022-09-12 20:42:19 +0100

    Categories list Added to sidebar/django-render-partial installed

--- context_processors/context_processors.py
+++ context_processors/context_processors.py
@@ -1,5 +1,6 @@
-from blog.models import Article
+from blog.models import Article,Category
 
 def recent_articles(request):
     recent_articles = Article.objects.order_by('-updated')
-    return ({'recent_articles':recent_articles})
+    categories = Category.objects.all()
+    return ({'recent_articles':recent_articles, 'categories':categories })
