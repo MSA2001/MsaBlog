058fa795ccbf26219c72a43074e132fe54e40184
Msa2001 <msa2001b21gmail.com>
2022-09-16 01:33:29 +0100

    Pagination DONE!

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Article,Category
+from django.core.paginator import Paginator
 # Create your views here.
 def post_detail(request,slug):
     article = get_object_or_404(Article,slug=slug)
     recent_articles=Article.objects.all().order_by('-updated')[:4]
     return render (request,'blog/post-details.html',{ 'article':article , 'recent_articles':recent_articles  })
+
 def articles_list(request):
-    articles = Article.objects.all()
-    return render(request,'blog/articles_list.html',{ 'articles':articles })
+    articles = Article.objects.all().order_by('-created')
+    page_number = request.GET.get('page')
+    paginator = Paginator(articles,2)
+    object_list = paginator.get_page(page_number)
+    return render(request,'blog/articles_list.html',{ 'articles':object_list })
 
 def category_detail(request,pk=None):
     category=Category.objects.get(id=pk)
