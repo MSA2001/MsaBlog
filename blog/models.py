0da8fa94634fc67dba1def06119510a8f5b9427d
Msa2001 <msa2001b21gmail.com>
2022-09-20 23:40:26 +0100

    Comments(1)

--- blog/models.py
+++ blog/models.py
@@ -36,3 +36,14 @@ class Article(models.Model):
     
     def __str__(self):
         return f"{self.title} - {self.body[:30]} "
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article,on_delete=models.CASCADE,related_name='comments')
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='comments')
+    parent =models.ForeignKey('self',null=True,blank=True,on_delete=models.CASCADE,related_name='replies')
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{ self.user.username  } - { self.body[:25] }"
