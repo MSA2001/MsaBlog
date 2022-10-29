854db76d3c6a39f02a1d7515ccd46a067d314d71
Msa2001 <msa2001b21gmail.com>
2022-10-29 17:07:39 +0100

    index articles order randomized

--- home/views.py
+++ home/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from blog.models import Article
 # Create your views here.
+
+
 def home(request):
-    articles = Article.objects.all()
-
-    recent_articles=Article.objects.all().order_by('-updated')[:4]
-
-    return render(request,'home/index.html',{ 'articles':articles , 'recent_articles':recent_articles })
+    articles = Article.objects.all().order_by('?')
+    recent_articles = Article.objects.all().order_by('-updated')[:4]
+    return render(request, 'home/index.html', {'articles': articles, 'recent_articles': recent_articles})
