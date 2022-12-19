d3c82c67aff95de0560412e3f45eff8ea55cdb37
Msa2001 <msa2001b21gmail.com>
2022-12-19 13:58:11 +0330

    L

--- Msablog/urls.py
+++ Msablog/urls.py
@@ -19,8 +19,8 @@ from . import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',include('home.urls')),
-    path('',include('accounts.urls')),
-    path('articles/',include('blog.urls')),
+    path('', include('home.urls')),
+    path('', include('accounts.urls')),
+    path('articles/', include('blog.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name='home'
 urlpatterns = [
-    path('',views.home,name='home')
+    path('', views.home, name='home')
 ]
 
 
