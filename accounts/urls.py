4066a422a9170c0cfffa276212a8d14968c389b6
Msa2001 <msa2001b21gmail.com>
2022-12-19 14:13:10 +0330

    Bugs fixed 1

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path("login", views.user_login, name='login'),
+    path('login', views.user_login, name='login'),
     path('logout', views.user_logout, name='logout'),
     path('register', views.user_register, name='register'),
-    path('edit', views.user_edit, name='edit')
+    path('edit', views.user_edit, name='edit'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,4 +6,4 @@ from blog.models import Article
 def home(request):
     articles = Article.objects.all().order_by('?')
     recent_articles = Article.objects.all().order_by('-updated')[:4]
-    return render(request, 'home/index.html', {'articles': articles, 'recent_articles': recent_articles})
+    return render(request, 'home/index-6.html', {'articles': articles, 'recent_articles': recent_articles})
